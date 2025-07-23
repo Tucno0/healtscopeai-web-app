018b9b09b4cf2e970f4d539d064d5f58adab89ac
Jhampier Tucno <yantucnoperez@gmail.com>
2025-07-23 15:46:55 -0500

    Add dynamic rendering configuration to PanelPage: prevent pre-rendering and set revalidation to zero.

--- src/app/(protected)/dashboard/(panel)/page.tsx
+++ src/app/(protected)/dashboard/(panel)/page.tsx
@@ -1,5 +1,9 @@
 import { PanelView } from '@/modules/dashboard/panel/ui/views/panel-view';
 
+// Configuración para evitar pre-renderizado
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 const PanelPage = () => {
   return <PanelView />;
 };
